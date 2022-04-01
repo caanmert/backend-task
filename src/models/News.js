9201eee8eb08d95ded3df7a4c6b28f34f738eabd
canmert <canmert.dev@gmail.com>
2022-04-01 16:17:34 +0300

    creating news schema

---
+++ src/models/News.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const NewsSchema = new Schema({
+
+  date: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+
+  },
+  description: {
+    type: String,
+    required: true,
+
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+
+});
+
+const News = mongoose.model('news', NewsSchema);
+
+export default News;
